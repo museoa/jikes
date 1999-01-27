0c55dfdedaf2ff5321c7636aa21462116e08e465
shields <>
1999-01-27 22:14:28 +0000

    Update for separate distribution with separate license

--- jikestst/derek/w/exec.d
+++ jikestst/derek/w/exec.d
@@ -1,7 +1,7 @@
-# This software is subject to the terms of the IBM Jikes Compiler
+# This software is subject to the terms of the IBM Jikes Test Suite
 # License Agreement available at the following URL:
 # http://www.ibm.com/research/jikes.
-# Copyright (C) 1996, 1998, International Business Machines Corporation
+# Copyright (C) 1996, 1999, International Business Machines Corporation
 # and others.  All Rights Reserved.
 # You must accept the terms of that agreement to use this software.
 #
