b71e931d7ccde1193cbb8a187f0a7c3c28d36e5c
shields <>
1999-02-03 22:11:04 +0000

    add tests from PRs 290..346

---
+++ jikestst/pr/src/pr319.java
@@ -0,0 +1,20 @@
+// $Id$
+// This software is subject to the terms of the IBM Jikes Test Suite
+// License Agreement available at the following URL:
+// http://www.ibm.com/research/jikes.
+// Copyright (C) 1996, 1999, International Business Machines Corporation
+// and others.  All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+
+package spoo;
+
+import java.util.Date;
+
+public class Tmp {
+
+	{
+		try {
+			Object obj = (new Date()).clone();
+		} catch (Exception e) {}
+	}
+}
---
+++ jikestst/pr/src/pr326.java
@@ -0,0 +1,16 @@
+// $Id$
+// This software is subject to the terms of the IBM Jikes Test Suite
+// License Agreement available at the following URL:
+// http://www.ibm.com/research/jikes.
+// Copyright (C) 1996, 1999, International Business Machines Corporation
+// and others.  All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+
+import java.awt.datatransfer.DataFlavor;
+
+public interface InterfaceBug
+{
+	public DataFlavor StringDataFlavor =
+										new DataFlavor(String.class, "String");
+}
+
