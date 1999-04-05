617c01e4d4d0f44a4192208a9102baeefabcc56b
shields <>
1999-04-05 18:47:50 +0000

    Add Id string to gencode.java

--- jikes/src/gencode.java
+++ jikes/src/gencode.java
@@ -7,6 +7,8 @@
 // and others.  All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 //
+// $Id:
+//
 // Generate code.h and code.cpp to reflect classification of Unicode
 // characters used by a Java Virtual Machine (usually Sun's, so that
 // Jikes and the Sun JVM use same classification).
