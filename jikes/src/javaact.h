e011cfd7f9a49213f824f41644d11dce689c653e
shields <>
1999-05-05 15:31:31 +0000

    update generated header files after java.g change

--- jikes/src/javaact.h
+++ jikes/src/javaact.h
@@ -2,7 +2,7 @@
 // This software is subject to the terms of the IBM Jikes Compiler Open
 // Source License Agreement available at the following URL:
 // http://www.ibm.com/research/jikes.
-// Copyright (C) 1996, 1998, International Business Machines Corporation
+// Copyright (C) 1996, 1999, International Business Machines Corporation
 // and others.  All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 //
