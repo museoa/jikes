9c27d1f75060f2bd952e0f036f13110f170d971f
shields <>
1999-11-04 14:02:23 +0000

    update to use IBM Public License Version 1.0

--- jikespg/examples/bnf/Parser.java
+++ jikespg/examples/bnf/Parser.java
@@ -1,5 +1,5 @@
 // $Id$
-// This software is subject to the terms of the IBM Jikes Parser Generator
+// This software is subject to the terms of the IBM Jikes Compiler
 // License Agreement available at the following URL:
 // http://www.ibm.com/research/jikes.
 // Copyright (C) 1996, 1998, International Business Machines Corporation
