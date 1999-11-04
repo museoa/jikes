4525b34abdd2eb0b1dc6e3241bc4e2ffdacca52e
shields <>
1999-11-04 15:01:21 +0000

    convert to IBM Public License Version 1.0

--- jikestst/pr/src/Test.java
+++ jikestst/pr/src/Test.java
@@ -1,4 +1,6 @@
-// This software is subject to the terms of the IBM Jikes Test Suite
+// $Id$
+//
+// This software is subject to the terms of the IBM Jikes Compiler
 // License Agreement available at the following URL:
 // http://www.ibm.com/research/jikes.
 // Copyright (C) 1996, 1999, International Business Machines Corporation
