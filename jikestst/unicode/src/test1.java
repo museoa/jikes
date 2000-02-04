85e777c359b5ac860461abf836f2d74ea5dd979f
lord <>
2000-02-04 02:40:53 +0000

    New 'unicode' test suite.
    Obtained from: Vadim Zaliva <lord@crocodile.org>
    Submitted by: Vadim Zaliva <lord@crocodile.org>
    Reviewed by: Vadim Zaliva <lord@crocodile.org>

---
+++ jikestst/unicode/src/test1.java
@@ -0,0 +1,47 @@
+/**
+ * Following program contains indentifiers in russian,
+ * in KOI8-R encoding.
+ * It should be compiled with '-encoding koi8-r' flag.
+ * 
+ * This code test following cases:
+ * 
+ * 1. Inner class name in koi8
+ * 2. instance variable name in koi8
+ * 
+ * When executed it rints 10 lines of russuan text, nubererd 0-9 and
+ * returns 0 upon sucessul execution.
+ * 
+ * Vadim Zaliva <lord@crocodile.org>
+ */
+public class test1
+{
+    private class ВнутреннийКласс
+    {
+        private int значение;
+
+        public ВнутреннийКласс(int значение)
+            {
+                this.значение=значение;
+            }
+        
+        int напечатать()
+            {
+                System.out.println("Значение :"+значение);
+                return значение;
+            }
+    };
+    
+    public static void main(String []args)
+    {
+        new test1();
+    }
+    
+    public test1()
+    {
+        ВнутреннийКласс к=new ВнутреннийКласс(23);
+        int res=к.напечатать();
+        System.exit(res==23?0:1);
+    }
+};
+
+
---
+++ jikestst/unicode/w/exec.d
@@ -0,0 +1,15 @@
+# $Id$
+# This software is subject to the terms of the IBM Jikes Compiler
+# License Agreement available at the following URL:
+# http://www.ibm.com/research/jikes.
+# Copyright (C) 1996, 1999, International Business Machines Corporation
+# and others.  All Rights Reserved.
+# You must accept the terms of that agreement to use this software.
+#
+#
+a -encoding koi8-r
+e test0 test0
+a -encoding koi8-r
+e test1 test1
+a -encoding koi8-r
+c test2
