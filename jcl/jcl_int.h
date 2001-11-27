e8573e524d4d0543a00b8ccfeb13e786bd9bd20a
mdejong <>
2001-11-27 07:04:46 +0000

    Imported jcl code into jcl module instead of jacl/src/jcl.

---
+++ jcl/jcl_int.h
@@ -0,0 +1,21 @@
+// $Id$
+//
+// This software is subject to the terms of the IBM Jikes Compiler
+// License Agreement available at the following URL:
+// http://ibm.com/developerworks/opensource/jikes.
+// Copyright (C) 1996, 1998, International Business Machines Corporation
+// and others.  All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+//
+#ifndef jcl_int_INCLUDED
+#define jcl_int_INCLUDED
+
+typedef unsigned char  u1;
+typedef unsigned short u2;
+typedef unsigned int   u4;
+
+typedef signed int     i4;
+typedef signed short   i2;
+typedef signed char    i1;
+
+#endif
