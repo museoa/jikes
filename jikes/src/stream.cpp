91fc7c5a8589d37df2b73607a1fb01547d407069
cabbey <>
2002-07-07 20:37:54 +0000

    fix error message

--- jikes/src/stream.cpp
+++ jikes/src/stream.cpp
@@ -62,7 +62,7 @@ const wchar_t *StreamError::getErrorMessage()
         return L"Invalid escape sequence.";
     case StreamError::DEPRECATED_IDENTIFIER_ASSERT:
         return L"The use of \"assert\" as an identifier is deprecated,"
-            L" as it is now a keyword. Use -source 1.4 if you intended to make use of "
+            L" as it is now a keyword. Use -source 1.4 if you intended "
             L" to make use of assertions.";
     case StreamError::DOLLAR_IN_IDENTIFIER:
         return L"The use of \"$\" in an identifier, while legal, is strongly "
