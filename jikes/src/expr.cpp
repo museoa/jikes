15c873763c2cc2e4e6a54a15575164b147ecdf33
ericb <>
2004-01-14 13:48:16 +0000

    2004-01-14  Eric Blake  <ebb9@byu.net>
    	* src/expr.cpp (ReportMethodNotFound): Improve error message when
    	accessing interface.clone(); it regressed from 1.18.
    	* src/config.h.in: Regenerate (missed checking this in yesterday).

--- jikes/src/expr.cpp
+++ jikes/src/expr.cpp
@@ -3,8 +3,7 @@
 // This software is subject to the terms of the IBM Jikes Compiler
 // License Agreement available at the following URL:
 // http://ibm.com/developerworks/opensource/jikes.
-// Copyright (C) 1996, 1998, 1999, 2000, 2001, 2002 International Business
-// Machines Corporation and others.  All Rights Reserved.
+// Copyright (C) 1996, 2004 IBM Corporation and others.  All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 //
 
@@ -263,8 +262,8 @@ void Semantic::ReportMethodNotFound(AstMethodInvocation* method_call,
                     // even though jikes treats interfaces as subtypes of
                     // Object.
                     //
-                    if (field_access && method -> ACC_PROTECTED() &&
-                        field_access -> base -> Type() -> ACC_INTERFACE())
+                    if (base && method -> ACC_PROTECTED() &&
+                        base -> Type() -> ACC_INTERFACE())
                     {
                         assert(method -> containing_type == control.Object());
                         ReportSemError(SemanticError::PROTECTED_INTERFACE_METHOD_NOT_ACCESSIBLE,
